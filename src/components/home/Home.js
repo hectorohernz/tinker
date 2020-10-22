1cfa6cfcd522bb5313321085ef7388284e26b07f
highsgod <hectorohernz@gmail.com>
2020-10-21 20:35:14 -0400

    home page responsive design

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,7 +8,15 @@ export default class Home extends Component{
             <>
                 <NavBar/>
                 <section className="home__introduction">
+                        <div className="HI__header">
+                            <h5>Share. Inspire.  Discover. Collaborate.</h5>
+                        </div>
+                 
+                    <div className="HI__image__container">
+                        <img src="./assets/pixeltrue-web-design.png" alt="A Man petting his pet dog." className="HI__image"/>
+                    </div>
 
+                    <button>Learn More</button>
                 </section>
             </>
         )
